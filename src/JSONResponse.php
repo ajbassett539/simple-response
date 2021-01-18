61c41ce32110a485bd8158eb413ee51babfd32d8
Alexandra <ajbassett539@gmail.com>
2021-01-18 10:12:52 -0500

    Removing Tab Chars

--- src/JSONResponse.php
+++ src/JSONResponse.php
@@ -1,16 +1,16 @@
 <?php
-	////////////////////////////////////////////////////////////////////////////
-	// SimpleResponse\JSONResponse
-	////////////////////////////////////////////////////////////////////////////
-	
-	namespace SimpleResponse;
+    ////////////////////////////////////////////////////////////////////////////
+    // SimpleResponse\JSONResponse
+    ////////////////////////////////////////////////////////////////////////////
+    
+    namespace SimpleResponse;
 
-	class JSONResponse {
-		public $data;
+    class JSONResponse {
+        public $data;
 
-		////////////////////////////////////////////////////////////////////////
-		function __construct($data = NULL, $exit = true, $extraHeaders = Array()) {
-			$this->data = $data;
+        ////////////////////////////////////////////////////////////////////////
+        function __construct($data = NULL, $exit = true, $extraHeaders = Array()) {
+            $this->data = $data;
             if( $exit ) {
                 if( !empty($extraHeaders) )
                     foreach( $extraHeaders AS $header ) 
@@ -18,11 +18,11 @@
                 header("Content-Type: application/json");
                 exit($this);
             }
-		}
-		
-		////////////////////////////////////////////////////////////////////////
-		public function __toString() {
-			$string = json_encode($this->data) . "";
-			return $string;
-		}
-	}
+        }
+        
+        ////////////////////////////////////////////////////////////////////////
+        public function __toString() {
+            $string = json_encode($this->data) . "";
+            return $string;
+        }
+    }
