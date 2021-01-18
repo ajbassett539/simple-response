fa8cc4072ee2109983b7cc97956c624edf98d830
Alexandra <ajbassett539@gmail.com>
2021-01-18 10:01:55 -0500

    Initial Commit

---
+++ src/Response.php
@@ -0,0 +1,28 @@
+<?php
+	////////////////////////////////////////////////////////////////////////////
+	// SimpleResponse\JSONResponse
+	////////////////////////////////////////////////////////////////////////////
+	
+	namespace SimpleResponse;
+
+	class JSONResponse {
+		public $data;
+
+		////////////////////////////////////////////////////////////////////////
+		function __construct($data = NULL, $exit = true, $extraHeaders = Array()) {
+			$this->data = $data;
+            if( $exit ) {
+                if( !empty($extraHeaders) )
+                    foreach( $extraHeaders AS $header ) 
+                        header($header);
+                header("Content-Type: application/json");
+                exit($this);
+            }
+		}
+		
+		////////////////////////////////////////////////////////////////////////
+		public function __toString() {
+			$string = json_encode($this->data) . "";
+			return $string;
+		}
+	}
